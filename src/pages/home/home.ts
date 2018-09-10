75a37b797bc6c30c566ecf68cf4d42f682f7eba9
Brian Calma <briancalamdevacc@gmail.com>
2018-09-10 16:31:57 +0800

    Finish Core TODO Functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,28 +8,40 @@ import { EditTodoListModalPage } from '../edit-todo-list-modal/edit-todo-list-mo
 })
 export class HomePage {
 
-  public items = [
-                    { title: "Todo 1", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
-                    { title: "Todo 2", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
-                    { title: "Todo 3", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
-                    { title: "Todo 4", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
-                    { title: "Todo 5", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
-                 ];
+  // public items = [
+  //                   { title: "Todo 1", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
+  //                   { title: "Todo 2", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
+  //                   { title: "Todo 3", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
+  //                   { title: "Todo 4", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
+  //                   { title: "Todo 5", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] },
+  //                ];
 
 
   public itemListTitle = "";
+
+  public activities = [
+    { 
+      title:"Activity 1" ,
+      tasks: [
+              { title: "Todo 1", description: "Some dummy text lorem ipsum dulum", status: false , subtasks:[] }
+            ],
+      tempStr: ""
+    }
+  ];
+
+  // public activities;
 
   constructor(public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController) {
 
   }
 
 
-  showEditTodoList(item, index) {
+  showEditTodoList(item, index, activityIndex) {
     
     let modal = this.modalCtrl.create(EditTodoListModalPage,{ data : item });
 
     modal.onDidDismiss(data => {
-      this.items[index] = data;
+      this.activities[activityIndex].tasks[index] = data;
     });
 
     modal.present();
@@ -46,7 +58,8 @@ export class HomePage {
         {
           text: 'Add',
           handler: data => {
-            console.log(data);
+            let newActivity = { title: data.activityTitle, tasks: [] , tempStr: ""};
+            this.activities.push(newActivity);
           }
         }
       ]
@@ -55,22 +68,51 @@ export class HomePage {
     alert.present();
   }
 
-  addNewItemInList() {
-    
-    let newItem = { title: this.itemListTitle, description: "", status: false, subtasks:[] };
-    
-    this.items.push(newItem);
+  showEditTodoPrompt(activityIndex) {
+    let alert = this.alertCtrl.create({
+      title: "Edit Activity",
+      inputs: [{ name: 'activityTitle', placeholder: '* Enter Title' , value: this.activities[activityIndex].title}],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Update',
+          handler: data => {
+            let activity = data.activityTitle;
 
-    this.itemListTitle = "";
+            this.activities[activityIndex].title = activity;
+            
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
 
-  finsihedListItem(i) {
-    let finishedItem = this.items[i];
+  addNewItemInList(activityIndex) {
+    
+    let newItem = { title: this.activities[activityIndex].tempStr, description: "", status: false, subtasks:[] };
+    
+    // this.items.push(newItem);
 
-    this.items.splice(i,1);
+    this.activities[activityIndex].tasks.push(newItem);
+    this.activities[activityIndex].tempStr = "";
+  }
 
-    this.items.push(finishedItem);
+
+  finsihedListItem(activityIndex, i) {
+    let finishedItem =  this.activities[activityIndex].tasks[i];
+
+    this.activities[activityIndex].tasks.splice(i,1);
+
+    this.activities[activityIndex].tasks.push(finishedItem);
+  }
+
+  deleteActivity(actIndex) {
+    this.activities.splice(actIndex, 1);
   }
 
 }
